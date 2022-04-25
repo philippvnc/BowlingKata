84d52cc471d271981ab53c0069223ecdb48029b5
Philipp von Neumann-Cosel <philipp.vonneumanncosel@gmail.com>
2022-04-25 14:19:51 +0200

    adds parametrized test for openFrame games with only same throws

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BowlingGameTest {
 
@@ -9,6 +11,15 @@ class BowlingGameTest {
     @BeforeEach
     public void setup() {
         bowlingGame = new BowlingGame();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void openFramesAllSameThrows(int throwValue) {
+        for (int frameNumber = 0; frameNumber < 10; frameNumber++) {
+            bowlingGame.addFrame(throwValue, throwValue);
+        }
+        Assertions.assertEquals(20 * throwValue, bowlingGame.getGameScore());
     }
 
     @Test
