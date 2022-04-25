ed33da98596d8ec6e7bfa7cdb36f104e7a3c91e1
Philipp von Neumann-Cosel <philipp.vonneumanncosel@gmail.com>
2022-04-25 13:45:50 +0200

    adds test for all ones game

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -11,4 +11,14 @@ class BowlingGameTest {
         }
         Assertions.assertEquals(0, bowlingGame.getGameScore());
     }
+
+    @Test
+    void AllOnes() {
+        BowlingGame bowlingGame = new BowlingGame();
+        for (int frameNumber = 0; frameNumber < 10; frameNumber++) {
+            bowlingGame.addFrame(1, 1);
+        }
+        Assertions.assertEquals(20, bowlingGame.getGameScore());
+    }
+
 }
