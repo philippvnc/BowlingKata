338490202dda3735ede800e5907e72c7f83b05b4
Philipp von Neumann-Cosel <philipp.vonneumanncosel@gmail.com>
2022-04-26 13:37:09 +0200

    satisfies FrameFactory StrikeFrame test

--- src/main/java/FrameFactory.java
+++ src/main/java/FrameFactory.java
@@ -1,7 +1,9 @@
 public class FrameFactory {
 
     public Frame getFrame(int firstThrow, int secondThrow) {
-        if (firstThrow + secondThrow == 10) {
+        if (firstThrow == 10) {
+            return new StrikeFrame(firstThrow, 0);
+        } else if (firstThrow + secondThrow == 10) {
             return new SpareFrame(firstThrow, secondThrow);
         } else {
             return new OpenFrame(firstThrow, secondThrow);
