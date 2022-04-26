96163ea7622ac5646bc870b9334f38dfbff2f745
Philipp von Neumann-Cosel <philipp.vonneumanncosel@gmail.com>
2022-04-26 12:40:23 +0200

    fixes throw value range for tests, (sum of pins was allowed to be > 10)

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -14,7 +14,7 @@ class BowlingGameTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    @ValueSource(ints = {0, 1, 2, 3, 4})
     public void openFramesAllSameThrows(int throwValue) {
         for (int frameNumber = 0; frameNumber < 10; frameNumber++) {
             bowlingGame.addFrame(throwValue, throwValue);
@@ -23,7 +23,7 @@ class BowlingGameTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    @ValueSource(ints = {0, 1, 2, 3, 4})
     public void spareAndOpenFramesAllSameThrows(int throwValue) {
         bowlingGame.addFrame(2, 8);
         for (int frameNumber = 0; frameNumber < 9; frameNumber++) {
