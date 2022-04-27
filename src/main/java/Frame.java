9b51e4090295db91b845209ea8afa3286d51f09f
Philipp von Neumann-Cosel <philipp.vonneumanncosel@gmail.com>
2022-04-27 11:35:57 +0200

    refactors Frame model, no need to store secondThrow value at all

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -2,8 +2,6 @@ public interface Frame {
 
     public int getFirstThrow();
 
-    public int getSecondThrow();
-
     public int getBaseScore();
 
     public int getFinalScore();
--- src/main/java/OpenFrame.java
+++ src/main/java/OpenFrame.java
@@ -1,23 +1,16 @@
 public class OpenFrame implements Frame {
 
     private int firstThrow;
-    private int secondThrow;
     private int baseScore;
 
     public OpenFrame(int firstThrow, int secondThrow) {
         this.firstThrow = firstThrow;
-        this.secondThrow = secondThrow;
         baseScore = firstThrow + secondThrow;
     }
 
     @Override
     public int getFirstThrow() {
         return firstThrow;
-    }
-
-    @Override
-    public int getSecondThrow() {
-        return secondThrow;
     }
 
     @Override
